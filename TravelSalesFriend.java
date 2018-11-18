5c2245b8529fec054aa715e0e69548af8fc7133b
hanglinzhou <hanglin.zhou@richmond.edu>
2018-11-18 15:52:34 -0500

    implement

--- TravelSalesFriend.java
+++ TravelSalesFriend.java
@@ -12,54 +12,88 @@ public class TravelSalesFriend {
     }
 
     //after reading the input
-    int minCost = getMinCost(n, flyCost);
+   //int[][] recover = new int[n][n];
+   int minCost = getMinCost(n, flyCost);
+   System.out.println(minCost);
   }
 
   public static int getMinCost(int n, int[][] flyCost) {
     // mc[i][j]: min cost if I stop at i and friend stop at j
     int[][] mc = new int[n][n];
-    
-    /* Base case:
-     *   mc[0][0] = 0; [probably all [a][a] should = -1]
-     *   
-     *   // I: 0, f: j
-     *   mc[0][1] = 0;
-     *   for (j = 2 ... n - 1)
-     *     mc[0][j] = mc[0][j - 1] + flyCost[j - 1][j];
-     *   // I: i; f: 0???
-     *   for (int i = 1 ... n - 1)
-     *     mc[i][0] = mc[i - 1][0] + flyCost[i - 1][i];
-     *
-     * Iterative:
-     *   for (i = 1 ... n - 1) {
-     *     for (j = 1 ... n - 1) {
-     *       if (i < j) {
-     *         if (j - i == 1) {
-     *           mc[i][j] = MIN(mc[i - 1][j] + flyCost[i - 1][i], //I fly from i - 1
-     *                          mc[i][j - 1] + flyCose[i - 1][j]) //F fly from i - 1
-     *         } else {
-     *           mc[i][j] = mc[i][j - 1] + flyCost[i][j - 1];
-     *         }
-     *       } else if (i > j) {
-     *         if (i - j == 1) {
-     *           mc[i][j] = MIN(mc[j - 1][j] + flyCost[j - 1][i], //I fly from j - 1
-     *                          mc[i][j - 1] + flyCost[j - 1][j]) //F fly from j - 1
-     *         } else {
-     *           mc[i][j] = mc[i - 1][j] + flyCost[i - 1][i];
-     *         }
-     *        // mc[i][j] = MIN(mc[j - 1][j] + flyCost[j - 1][i]//I at j - 1,
-     *                       // mc[i][j - 1] + flyCost[j - 1][j]//F at j - 1);
-     *       } else {
-     *         // i == j, but do what?
-     *       }
-     *     }
-     *   }
-     *
-     * Return:
-         return //return what????
-         return min(mc[i = 0 ... n - 2][n - 1], mc[n - 1][0...n - 2]);
-     */
+    //Base case
+    mc[0][0] = 0;
+    mc[0][1] = 0;
 
-    return 0;
+    //I stay, friend flies
+    for (int j = 2; j < n; j++){
+      mc[0][j] = mc[0][j-1] + flyCost[j-1][j];
+    }
+
+    //friend stays, I fly
+    for (int i = 1; i < n; i++){
+      mc[i][0] = mc[i-1][0] + flyCost[i-1][i];
+    }
+
+
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < n; j++) {
+        if (i < j) {
+          if (j - i == 1) {
+            int min = Integer.MAX_VALUE;
+            for (int k = 0; k < i; k++){
+              if (k == 1) continue;
+
+              if (min > ( mc[i][k] + flyCost[k][j] )){
+                min = mc[i][k] + flyCost[k][j];
+                //recover[i][j] = min;
+              }
+            }
+            //mc[i][j] = Math.min(mc[i - 1][j] + flyCost[i - 1][i], //I fly from i - 1
+              //`                 mc[i][i - 1] + flyCost[i - 1][j]); //F fly from i - 1
+            }
+
+          else {
+            mc[i][j] = mc[i][j - 1] + flyCost[j-1][j];
+          }
+        }
+
+        else if (i > j) {
+          if (i - j == 1) {
+            int min = Integer.MAX_VALUE;
+            for (int k = 0; k < j; k++){
+              if (k == 1) continue;
+              if (min > ( mc[k][j] + flyCost[k][i] )){
+                min = mc[k][j] + flyCost[k][i];
+                //recover[i][j] = min;
+              }
+            }
+          }
+          else {
+            mc[i][j] = mc[i - 1][j] + flyCost[i - 1][i];
+          }
+        }
+        //else {
+              // i == j, but do what?
+        //}
+      }
+    }
+    for (int[] j : mc){
+      for (int i : j){
+        System.out.println(i);
+      }
+    }
+    int min = Integer.MAX_VALUE;
+    //System.out.println("Friend ends");
+    for (int i = 0; i < n - 1; i++) {
+      min = (mc[i][n-1] < min) ? mc[i][n-1] : min;
+      //System.out.println(mc[i][n-1]);
+    }
+
+    //System.out.println("I end");
+    for (int j = 0; j < n - 1; j++) {
+      min = (mc[n-1][j] < min) ? mc[n-1][j] : min;
+      //System.out.println(mc[n-1][j]);
+    }
+      return min;
   }
 }
